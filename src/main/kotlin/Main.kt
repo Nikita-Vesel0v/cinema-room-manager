c95322985ed0cefddc5af7b1775479636ab2e17f
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-08 14:05:45 +0300

    can calculate income of sold-out room

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,42 @@
-package org.example
+package cinema
+
+class CinemaRoomManager {
+    private var profit = 0
+    private var rows = 0
+    private var columns = 0
+    private var room = emptyList<List<Char>>()
+
+    init {
+        println("Enter the number of rows:")
+        rows = readln().toInt()
+        println("Enter the number of seats in each row:")
+        columns = readln().toInt()
+        room = List(rows) { List(columns) { 'S' } }
+    }
+
+    fun getTotalIncome(): Int {
+        print("\n\nTotal income:\n$")
+        return profit
+    }
+    fun printRoom() {
+        print("\nCinema:\n  ")
+        var i = 1; repeat(columns) { print("${i++} ") }
+        room.forEachIndexed { index, row -> print("\n${index + 1} ${row.joinToString(" ")}") }
+    }
+
+    fun calcProfit() {
+        profit = columns * if (rows * columns > 60) {
+            (rows / 2) * 10 + (rows - rows / 2) * 8
+        }
+        else {
+            rows * 10
+        }
+    }
+}
 
 fun main() {
-    print("Cinema:\n  ")
-    var i = 1
-    repeat(8) { print("${i++} ") }
-    println()
-    i = 1
-    repeat(7) { println("${i++} ${"S ".repeat(8)}")}
-
+    val cinemaRoomManager = CinemaRoomManager()
+    cinemaRoomManager.printRoom()
+    cinemaRoomManager.calcProfit()
+    println(cinemaRoomManager.getTotalIncome())
 }
